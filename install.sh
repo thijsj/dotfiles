ec0d4d9d7df20d2d3b92b3e39bcdd2f4c3d1e040
Thijs Janssen <thijs@thijsj.nl>
2024-08-09 19:19:28 +0200

    TJ: install config files

--- install.sh
+++ install.sh
@@ -7,10 +7,18 @@ function config_link () {
   local src=${mydir}/config_${name}
   local dest=${HOME}/.config/${name}
   test -d ${HOME}/.config || mkdir ${HOME}/.config
-  if [ -d ${dest} ]; then
+  if [ -d ${dest} ] && [ ! -L ${dest} ]; then
     echo "Directory ${dest} exists, not removing"
     return
   fi
+  if [ -L ${dest} ]; then
+    if [ "$(readlink ${dest})" == "${src}" ]; then
+      echo "'${name}' is already configured"
+      return
+    fi
+  fi
+  ln -snf ${src} ${dest}
+  echo "Configured '${name}'"
 }
 
 config_link kitty
